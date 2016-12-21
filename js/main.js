1039d70366790556792831a9a69d5cf4940ddf07
vikok <vikskopkop@gmail.com>
2016-12-21 11:47:07 +0200

    add new

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
 ;(function($) {
 	"use strict";
 
+$('.ba-slider').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.ba-type-slider'
+});
+$('.ba-type-slider').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  asNavFor: '.ba-cars-slider',
+  dots: true,
+  centerMode: true,
+  focusOnSelect: true
+});
+
 })(jQuery);
